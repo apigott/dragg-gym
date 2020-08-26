5b8859b96d1303a89337668a7697dafeabc70fe7
apigott <aisling.pigott@colorado.edu>
2020-08-26 14:03:38 -0600

    updated with stable baselines

--- dragg_gym/envs/dragg_env.py
+++ dragg_gym/envs/dragg_env.py
@@ -23,25 +23,26 @@ class DRAGGEnv(gym.Env):
         self.curr_step = -1
         self.action_episode_memory = []
 
-        self.MIN_PRICE = -0.2
-        self.MAX_PRICE = 0.2
+        self.MIN_PRICE = -1.0
+        self.MAX_PRICE = 1.0
         # self.action_space = spaces.Discrete(41)
-        self.action_space = spaces.Box(np.float32(np.array([-1])), np.float32(np.array([1])))
-        obs_low = np.array([[0], # current load
-                                        # [0], # forecasted load
-                                        # [0], # time of day
-                                        # [0] # predicted load over next X hours
-                                        ])
-        print(obs_low.shape)
-        obs_high = np.array([[self.agg.config['community']['total_number_homes'][0] * 15], # current load
-                                        # [self.agg.config['community']['total_number_homes'][0] * 15], # forecasted load
-                                        # [24], # time of day
-                                        # [self.agg.config['community']['total_number_homes'][0] * 100], # predicted load over next X hours
-                                        ])
+        self.action_space = spaces.Box(np.float32(np.array([self.MIN_PRICE])), np.float32(np.array([self.MAX_PRICE])))
+        obs_low = np.array([0, # current load
+                            0, # forecasted load
+                            0 # time of day
+                            # 0 # predicted load over next X hours
+                            ], dtype=np.float32)
+        obs_high = np.array([self.agg.config['community']['total_number_homes'][0] * 15, # current load
+                                        self.agg.config['community']['total_number_homes'][0] * 15, # forecasted load
+                                        24 # time of day
+                                        # self.agg.config['community']['total_number_homes'][0] * 100, # predicted load over next X hours
+                                        ], dtype=np.float32)
         self.observation_space = spaces.Box(obs_low, obs_high)
 
     def _get_reward(self, obs):
+        # self.agg.avg_load += 0.5*(self.agg.agg_load - self.agg.avg_load)
         reward = -1*(self.agg.agg_setpoint - self.agg.agg_load)**2
+        # reward = -1 * (self.agg.agg_load - self.agg.avg_load)**2
         return reward
 
     def _take_action(self, action):
@@ -54,10 +55,7 @@ class DRAGGEnv(gym.Env):
         self.agg.collect_data()
 
     def _get_state(self):
-        self.reward_price[0] = 0
-        forecast = self.agg._gen_forecast()
-        # return (self.agg.agg_load, forecast[0], self.agg.timestep / self.agg.dt, sum(forecast))
-        return self.agg.agg_load
+        return np.array([self.agg.agg_load, np.sum(self.agg.forecast_load), self.agg.timestep % self.agg.dt])
 
     def step(self, action): # done
         self.curr_step += 1
@@ -65,7 +63,7 @@ class DRAGGEnv(gym.Env):
         obs = self._get_state()
         reward = self._get_reward(obs)
         done = False
-        return [[obs]], reward, done, {}
+        return obs, reward, done, {}
 
     def reset(self):
         self.curr_step = -1
