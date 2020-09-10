31ddb564a4de8076cc5d889ed40476131ca3d069
Aisling Pigott <aipi0122@login12.rc.int.colorado.edu>
2020-09-10 09:14:02 -0600

    for batch runs on summit

---
+++ tests/run_slurm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --nodes=1  #Allocate whatever you need here 
+#SBATCH --cpus-per-task=10  #Allocate whatever you need here 
+#SBATCH --output=run.out
+#SBATCH --job-name=test
+#SBATCH --time=0-12:00:00
+#SBATCH --mail-user=aipi0122@colorado.edu
+#SBATCH --mail-type=ALL
+
+module purge
+export DATA_DIR='/projects/aipi0122/dragg/dragg/data'
+export LOGLEVEL='WARN'
+source /curc/sw/anaconda3/2019.07/bin/activate
+conda activate dragg
+redis-server --daemonize yes
+python -u main.py
+
