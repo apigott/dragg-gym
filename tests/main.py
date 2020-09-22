a92b97886ac16daa394fb2673eb2e78cdac1150d
apigott <aisling.pigott@colorado.edu>
2020-09-21 22:57:18 -0600

    sac

--- tests/main.py
+++ tests/main.py
@@ -5,22 +5,23 @@ import dragg_gym
 from dragg.aggregator import Aggregator
 
 import tensorflow as tf
-from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy, ActorCriticPolicy
+# from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy, ActorCriticPolicy
+from stable_baselines.sac.policies import LnMlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2, A2C, SAC, HER
 
-model_name = '8hr'
+model_name = '6hr_sac'
 
-# env = gym.make('dragg-v0')
-env = DummyVecEnv([lambda: gym.make('dragg-v0')])
-model = PPO2(MlpLnLstmPolicy, env, nminibatches=1, verbose=1, tensorboard_log="tensorboard_logs")
+env = gym.make('dragg-v0')
+# env = DummyVecEnv([lambda: gym.make('dragg-v0')])
+# model = PPO2(MlpLnLstmPolicy, env, nminibatches=1, verbose=1, tensorboard_log="tensorboard_logs")
 
-# model = SAC(MlpLnLstmPolicy, env, n_env=1, n_batch=1, n_steps=5000, verbose=1, tensorboard_log=".tensorboard_logs")
+model = SAC(LnMlpPolicy, env, verbose=1, tensorboard_log="tensorboard_logs")
 # model_class = SAC
 # goal_selection_strategy = 'future'
 # model = HER('MlpLnLstmPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy, verbose=1)
 
-model.learn(total_timesteps=5000, tb_log_name=model_name)
+model.learn(total_timesteps=5000, log_interval=10, tb_log_name=model_name)
 model.save(model_name)
 # model = PPO2.load(model_name)
 # model.set_env(env)
