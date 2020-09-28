0705c61eef2df47d361cddd37c56d05002ecf9c5
apigott <aisling.pigott@colorado.edu>
2020-09-28 12:06:48 -0600

    with pv

--- tests/main.py
+++ tests/main.py
@@ -13,54 +13,49 @@ from stable_baselines.sac.policies import LnMlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2, A2C, SAC, HER
 
-model_name = '6hr_sac-normalizedR'
+model_name = 'with_pv'
 
 env = gym.make('dragg-v0')
 env._max_episode_steps = 1000
-# env = DummyVecEnv([lambda: gym.make('dragg-v0')])
-# model = PPO2(MlpLnLstmPolicy, env, nminibatches=1, verbose=1, tensorboard_log="tensorboard_logs")
+# env = DummyVecEnv([lambda: gym.make('dragg-v0')]) # for models that require a vectorized env
 
-# model = SAC(LnMlpPolicy, env, verbose=1, tensorboard_log="tensorboard_logs")
-# model_class = SAC
-# goal_selection_strategy = 'future'
-# model = HER('MlpLnLstmPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy, verbose=1)
+model.learn(total_timesteps=5000, tb_log_name=model_name)
+model.save(model_name)
 
-# model.learn(total_timesteps=5000, tb_log_name=model_name)
-# model.save(model_name)
 # model = SAC.load(model_name)
 # model.set_env(env)
-for i in range(5):
+
+obs = env.reset()
+for _ in range(240):
+    action, _state = model.predict(obs)
+    # action = 0
+    obs, reward, done, info = env.step(action)
+
+# for i in range(5):
+#     temp_name = model_name+str(i)
+
 #     model.learn(total_timesteps=5000, tb_log_name=(model_name+str(i)))
 #     model.save(model_name+str(i))
-    temp_name = model_name+str(i)
 
-    data_dir = os.path.expanduser(os.environ.get('DATA_DIR','data'))
-    config_file = os.path.join(data_dir, os.environ.get('CONFIG_FILE', 'config.toml'))
-    with open(config_file,'r') as f:
-        data = toml.load(f)
-
-    data['rl']['version'] = temp_name
-    with open(config_file,'w') as f:
-        toml.dump(data, f)
-
-    env = gym.make('dragg-v0')
-
-    model = SAC.load(temp_name)
-
-# obs = env.reset()
-# for _ in range(240):
-#     action, _state = model.predict(obs)
-#     obs, reward, done, info = env.step(action)
-# env.agg.write_outputs(inc_rl_agents=False)
-
-    obs = env.reset()
-    state = None
-    done = [False for _ in range(1)]
-    for _ in range(240):
-        action, state = model.predict(obs, state=state, mask=done)
-        # action = 0
-        obs, reward , done, info = env.step(action)
-# env.write_outputs(inc_rl_agents=False)
-
-# r = Reformat()
-# r.tf_main()
+#
+#     data_dir = os.path.expanduser(os.environ.get('DATA_DIR','data'))
+#     config_file = os.path.join(data_dir, os.environ.get('CONFIG_FILE', 'config.toml'))
+#     with open(config_file,'r') as f:
+#         data = toml.load(f)
+#
+#     data['rl']['version'] = temp_name
+#     with open(config_file,'w') as f:
+#         toml.dump(data, f)
+#
+#     env = gym.make('dragg-v0')
+#
+#     model = SAC.load(temp_name)
+#     model.set_env(env)
+#
+#     obs = env.reset()
+#     state = None
+#     done = [False for _ in range(1)]
+#     for _ in range(240):
+#         action, state = model.predict(obs, state=state, mask=done)
+#         # action = 0
+#         obs, reward , done, info = env.step(action)
