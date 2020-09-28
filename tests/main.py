5faac52ef94d9452c00834f90322d90bd27b4d37
apigott <aisling.pigott@colorado.edu>
2020-09-28 17:08:03 -0600

    pv_batt

--- tests/main.py
+++ tests/main.py
@@ -13,30 +13,34 @@ from stable_baselines.sac.policies import LnMlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2, A2C, SAC, HER
 
-model_name = 'with_pv'
+model_name = 'with_pv_batt'
 
 env = gym.make('dragg-v0')
 env._max_episode_steps = 1000
-# env = DummyVecEnv([lambda: gym.make('dragg-v0')]) # for models that require a vectorized env
+# env = DummyVecEnv([lambda: gym.make('dragg-v0')])
+# model = PPO2(MlpLnLstmPolicy, env, nminibatches=1, verbose=1, tensorboard_log="tensorboard_logs")
+
+model = SAC(LnMl pPolicy, env, verbose=1, tensorboard_log="tensorboard_logs")
+# model_class = SAC
+# goal_selection_strategy = 'future'
+# model = HER('MlpLnLstmPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy, verbose=1)
 
 model.learn(total_timesteps=5000, tb_log_name=model_name)
 model.save(model_name)
-
 # model = SAC.load(model_name)
 # model.set_env(env)
 
 obs = env.reset()
 for _ in range(240):
-    action, _state = model.predict(obs)
-    # action = 0
+    # action, _state = model.predict(obs)
+    action = 0
     obs, reward, done, info = env.step(action)
+env.agg.write_outputs(inc_rl_agents=False)
 
 # for i in range(5):
-#     temp_name = model_name+str(i)
-
 #     model.learn(total_timesteps=5000, tb_log_name=(model_name+str(i)))
 #     model.save(model_name+str(i))
-
+#     temp_name = model_name+str(i)
 #
 #     data_dir = os.path.expanduser(os.environ.get('DATA_DIR','data'))
 #     config_file = os.path.join(data_dir, os.environ.get('CONFIG_FILE', 'config.toml'))
@@ -49,8 +53,7 @@ for _ in range(240):
 #
 #     env = gym.make('dragg-v0')
 #
-#     model = SAC.load(temp_name)
-#     model.set_env(env)
+#     SAC.load(temp_name)
 #
 #     obs = env.reset()
 #     state = None
@@ -59,3 +62,7 @@ for _ in range(240):
 #         action, state = model.predict(obs, state=state, mask=done)
 #         # action = 0
 #         obs, reward , done, info = env.step(action)
+# env.write_outputs(inc_rl_agents=False)
+
+# r = Reformat()
+# r.tf_main()
