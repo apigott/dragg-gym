8243108056ddae03b59127dcb3df093e79457777
apigott <aisling.pigott@colorado.edu>
2020-09-28 08:42:10 -0600

    multiple envs

--- tests/main.py
+++ tests/main.py
@@ -1,3 +1,6 @@
+import os
+import toml
+
 import dragg
 from dragg.reformat import Reformat
 import gym
@@ -13,24 +16,33 @@ from stable_baselines import PPO2, A2C, SAC, HER
 model_name = '6hr_sac-normalizedR'
 
 env = gym.make('dragg-v0')
-print("default max env steps", env._max_episode_steps)
 env._max_episode_steps = 1000
-print("new max env steps", env._max_episode_steps)
 # env = DummyVecEnv([lambda: gym.make('dragg-v0')])
 # model = PPO2(MlpLnLstmPolicy, env, nminibatches=1, verbose=1, tensorboard_log="tensorboard_logs")
 
-model = SAC(LnMlpPolicy, env, verbose=1, tensorboard_log="tensorboard_logs")
+# model = SAC(LnMlpPolicy, env, verbose=1, tensorboard_log="tensorboard_logs")
 # model_class = SAC
 # goal_selection_strategy = 'future'
 # model = HER('MlpLnLstmPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy, verbose=1)
 
 # model.learn(total_timesteps=5000, tb_log_name=model_name)
 # model.save(model_name)
-model = PPO2.load(model_name)
-model.set_env(env)
+# model = SAC.load(model_name)
+# model.set_env(env)
 for i in range(5):
-    model.learn(total_timesteps=5000, tb_log_name=(model_name+str(i)))
-    model.save(model_name+str(i))
+#     model.learn(total_timesteps=5000, tb_log_name=(model_name+str(i)))
+#     model.save(model_name+str(i))
+    data_dir = os.path.expanduser(os.environ.get('DATA_DIR','data'))
+    config_file = os.path.join(data_dir, os.environ.get('CONFIG_FILE', 'config.toml'))
+    with open(config_file,'r+') as f:
+        data = toml.load(f)
+        data['rl']['version'] = temp_name
+        toml.dump(dump, f)
+
+    env = gym.make('dragg-v0')
+
+    temp_name = model_name+str(i)
+    SAC.load(temp_name)
 
 # obs = env.reset()
 # for _ in range(240):
@@ -38,13 +50,13 @@ for i in range(5):
 #     obs, reward, done, info = env.step(action)
 # env.agg.write_outputs(inc_rl_agents=False)
 
-obs = env.reset()
-state = None
-done = [False for _ in range(1)]
-for _ in range(240):
-    action, state = model.predict(obs, state=state, mask=done)
-    # action = 0
-    obs, reward , done, info = env.step(action)
+    obs = env.reset()
+    state = None
+    done = [False for _ in range(1)]
+    for _ in range(240):
+        action, state = model.predict(obs, state=state, mask=done)
+        # action = 0
+        obs, reward , done, info = env.step(action)
 # env.write_outputs(inc_rl_agents=False)
 
 # r = Reformat()
