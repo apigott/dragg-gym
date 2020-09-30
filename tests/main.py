972a7d7ed07feb46fa209c172981f91a1e042be6
apigott <aisling.pigott@colorado.edu>
2020-09-30 09:24:55 -0600

    multiple alphas

--- tests/main.py
+++ tests/main.py
@@ -21,8 +21,8 @@ env = gym.make('dragg-v0')
 # model = SAC(LnMlpPolicy, env, learning_rate=0.03 , verbose=1, tensorboard_log="tensorboard_logs")
 # model.learn(total_timesteps=5000, tb_log_name=model_name)
 # model.save(model_name)
-model = SAC.load(model_name)
-model.set_env(env)
+# model = SAC.load(model_name)
+# model.set_env(env)
 #
 # obs = env.reset()
 # for _ in range(240):
@@ -31,10 +31,10 @@ model.set_env(env)
 #     obs, reward, done, info = env.step(action)
 # env.agg.write_outputs(inc_rl_agents=False)
 
-# alphas = [1,5,7,9]
-for i in range(5):
-# for a in alphas:
-    temp_name = model_name+"0"+str(a)
+alphas = [1,5,7,9]
+# for i in range(5):
+for a in alphas:
+    temp_name = model_name+"0"+str(i)
 
     data_dir = os.path.expanduser(os.environ.get('DATA_DIR','data'))
     config_file = os.path.join(data_dir, os.environ.get('CONFIG_FILE', 'config.toml'))
@@ -46,12 +46,12 @@ for i in range(5):
         toml.dump(data, f)
 
     # env = gym.make('dragg-v0')
-    # model = SAC(LnMlpPolicy, env, learning_rate=0.03 , verbose=1, tensorboard_log="tensorboard_logs")
-    model.learn(total_timesteps=5000, tb_log_name=(temp_name))
-    model.save(temp_name)
+    model = SAC(LnMlpPolicy, env, learning_rate=(0.01 * a) , verbose=1, tensorboard_log="tensorboard_logs")
+    # model.learn(total_timesteps=5000, tb_log_name=(temp_name))
+    # model.save(temp_name)
     # SAC.load(temp_name)
 
     obs = env.reset()
     for _ in range(240):
-        action, state = model.predict(obs, state=state, mask=done)
+        action, state = model.predict(obs)
         obs, reward , done, info = env.step(action)
