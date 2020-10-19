54d4b5da9f1553cfabc58d16b3f3819ba4465d70
apigott <aisling.pigott@colorado.edu>
2020-10-19 09:38:12 -0600

    rl with ev waterdraws

--- tests/main.py
+++ tests/main.py
@@ -13,7 +13,7 @@ from stable_baselines import PPO2, A2C, SAC, HER
 
 log = Logger("main")
 
-model_name = 'test'
+model_name = 'rl-wd-v2'
 
 data_dir = os.path.expanduser(os.environ.get('DATA_DIR','data'))
 config_file = os.path.join(data_dir, os.environ.get('CONFIG_FILE', 'config.toml'))
@@ -27,17 +27,17 @@ with open(config_file,'w') as f:
 env = gym.make('dragg-v0')
 env._max_episode_steps = 1000
 
-# model = SAC(LnMlpPolicy, env, learning_rate=0.03, verbose=1, tensorboard_log="tensorboard_logs")
-# model.learn(total_timesteps=5000, tb_log_name=model_name)
-# model.save(model_name)
+model = SAC(LnMlpPolicy, env, learning_rate=0.03, verbose=1, tensorboard_log="tensorboard_logs")
+model.learn(total_timesteps=5000, tb_log_name=model_name)
+model.save(model_name)
 
 # model = SAC.load('more_data')
 # model.set_env(env)
 
 obs = env.reset()
 for _ in range(240):
-    # action, _state = model.predict(obs)
-    action = 0
+    action, _state = model.predict(obs)
+    # action = 0
     obs, reward, done, info = env.step(action)
 
 # alphas = [3,5,7,9]
