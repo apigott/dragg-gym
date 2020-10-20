5cea33afb36b093f31af25cd6fad63d78e2fb9cd
apigott <aisling.pigott@colorado.edu>
2020-10-20 09:27:55 -0600

    sm waterdraws - rl

--- tests/main.py
+++ tests/main.py
@@ -13,31 +13,31 @@ from stable_baselines import PPO2, A2C, SAC, HER
 
 log = Logger("main")
 
-model_name = 'rl-wd-v2'
+model_name = 'rl-sm_waterdraws'
 
 data_dir = os.path.expanduser(os.environ.get('DATA_DIR','data'))
 config_file = os.path.join(data_dir, os.environ.get('CONFIG_FILE', 'config.toml'))
 with open(config_file,'r') as f:
     data = toml.load(f)
 
-data['rl']['version'] = [model_name]
+data['rl']['version'] = [model_name] + data['rl']['version']
 with open(config_file,'w') as f:
     toml.dump(data, f)
 
 env = gym.make('dragg-v0')
 env._max_episode_steps = 1000
 
-model = SAC(LnMlpPolicy, env, learning_rate=0.03, verbose=1, tensorboard_log="tensorboard_logs")
-model.learn(total_timesteps=5000, tb_log_name=model_name)
-model.save(model_name)
+# model = SAC(LnMlpPolicy, env, learning_rate=0.03, verbose=1, tensorboard_log="tensorboard_logs")
+# model.learn(total_timesteps=5000, tb_log_name=model_name)
+# model.save(model_name)
 
 # model = SAC.load('more_data')
 # model.set_env(env)
 
 obs = env.reset()
 for _ in range(240):
-    action, _state = model.predict(obs)
-    # action = 0
+    # action, _state = model.predict(obs)
+    action = 0
     obs, reward, done, info = env.step(action)
 
 # alphas = [3,5,7,9]
