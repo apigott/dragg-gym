c4c8bd8d8995791e8f7c19b92f7b2454f2ebad57
apigott <aisling.pigott@colorado.edu>
2020-11-01 21:08:45 -0700

    typo :)

--- dragg_gym/envs/dragg_env.py
+++ dragg_gym/envs/dragg_env.py
@@ -74,7 +74,7 @@ class DRAGGEnv(gym.Env):
 
     def get_reward(self, obs):
         sp = self.agg.agg_setpoint
-        reward = -1*(sp - self.agg.agg_load)**2 - self.agg.lam*np.clip((self.agg.max_load - (5 * self.agg.config['community']['total_number_homes'])),0,None)
+        reward = -1*(sp - self.agg.agg_load)**2 - self.agg.lam*np.clip((self.agg.max_load - (5 * self.agg.config['community']['total_number_homes'][0])),0,None)
         print("REWARD BEFORE NORM", reward)
         reward = (reward - self.agg.n_avg_reward) / (self.agg.n_max_reward - self.agg.n_min_reward)
         print("REWARD AFTER NORM", reward)
